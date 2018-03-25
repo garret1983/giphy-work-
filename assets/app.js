f5c08de3c028ec616fc73e4d9204912cc57f65b3
garret <garretgolledge@gmail.com>
2018-03-25 11:03:14 -0700

    worked on js

---
+++ assets/app.js
@@ -0,0 +1,70 @@
+var random = [""];
+
+
+function renderButtons() {
+    $("#giphy-button").empty();
+    for (var i = 0; i < random.length; i++) {
+        var a = $("<button>");
+        a.addClass("btn btn-default");
+
+        a.attr("data-name", random[i]);
+        a.text(random[i]);
+        $("#giphy-button").append(a);
+        console.log(random)
+    }
+}
+
+$("#giphy-button").on("click", function (event) {
+    event.preventDefault();
+    var newRandom = $("#random-input").val();
+    random.push(newRandom);
+
+    renderButtons();
+    console.log(newRandom)
+});
+
+//renderButtons();
+
+$("body").on("click", "btn", function () {
+    var search = $(this).attr("data-nme");
+    console.log($(this).attr("data-name"));
+    var queryURL = "https://api.giphy.com/v1/gifs/random?" + search + "api_key=4CDx7xbKWsPIV73Lc0j0ATCZhQPbML25&tag=&rating=G";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response); 
+
+        for (var j = 0; j < response.data.length; j++) {
+            var gifDiv = $("<div>");
+            var animateUrl = response.data[j].images.original.url;
+            var stillUrl = response.data[j].images.original_still.url;
+            var gifRating = response.data[j].rating;
+            console.log(stillUrl);
+
+            var gifHolder = $("<img>");
+            gifHolder.attr("src", stillUrl);
+            gifHolder.attr("alt", "giphy");
+            gifHolder.attr("data-animate", animateUrl);
+            gifHolder.attr("data-still", stillUrl);
+            gifHolder.attr("data-state", "still");
+            gifDiv.prepend(gifHolder);
+            gifDiv.prepend("<div> Rating: " + gifRating + "</div>");
+            $("#gifs-go-here").prepend(gifDiv);
+        }
+
+        $("body").on("click", "img", function () {
+            console.log($(this).attr("data-name"));
+            var state = $(this).attr("data-state");
+            if (state === 'still') {
+                $(this).attr('src', $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+
+            }
+            then();
+        })
+
+    })
+
+});
