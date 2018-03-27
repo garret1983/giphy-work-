dbb37335c4e372ffc03331a8afb1bf1ba0f85f87
garret <garretgolledge@gmail.com>
2018-03-26 19:57:40 -0700

    script changes

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,14 @@
-var random = [""];
+
+
+
+var random = ["randomGif"];
 
 
 function renderButtons() {
     $("#giphy-button").empty();
     for (var i = 0; i < random.length; i++) {
         var a = $("<button>");
-        a.addClass("btn btn-default");
+        a.addClass("btn btn- searchTerms");
 
         a.attr("data-name", random[i]);
         a.text(random[i]);
@@ -14,27 +17,29 @@ function renderButtons() {
     }
 }
 
-$("#giphy-button").on("click", function (event) {
+
+
+$("#add-random").on(".searchTerms", function () {
     event.preventDefault();
     var newRandom = $("#random-input").val();
     random.push(newRandom);
 
     renderButtons();
-    console.log(newRandom)
+    //console.log(newRandom)
 });
 
-//renderButtons();
+
 
 $("body").on("click", "btn", function () {
-    var search = $(this).attr("data-nme");
-    console.log($(this).attr("data-name"));
-    var queryURL = "https://api.giphy.com/v1/gifs/random?" + search + "api_key=4CDx7xbKWsPIV73Lc0j0ATCZhQPbML25&tag=&rating=G";
+    var search = $(this).attr("randomGif"); //might be problem 
+    console.log($(this).attr("randomGif"));//might be problem 
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=4CDx7xbKWsPIV73Lc0j0ATCZhQPbML25&tag=funny&rating=G";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response); 
+        console.log(response);
 
         for (var j = 0; j < response.data.length; j++) {
             var gifDiv = $("<div>");
@@ -42,13 +47,16 @@ $("body").on("click", "btn", function () {
             var stillUrl = response.data[j].images.original_still.url;
             var gifRating = response.data[j].rating;
             console.log(stillUrl);
-
+        
+            var gifTitle = response.data[j].title; 
             var gifHolder = $("<img>");
             gifHolder.attr("src", stillUrl);
             gifHolder.attr("alt", "giphy");
             gifHolder.attr("data-animate", animateUrl);
             gifHolder.attr("data-still", stillUrl);
             gifHolder.attr("data-state", "still");
+
+
             gifDiv.prepend(gifHolder);
             gifDiv.prepend("<div> Rating: " + gifRating + "</div>");
             $("#gifs-go-here").prepend(gifDiv);
